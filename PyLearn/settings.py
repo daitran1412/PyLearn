08dc74163c2c8797fb280f9e7a6335b9b1aecc65
hoangduong-hus <phamhoangduong_t64@hus.edu.vn>
2021-12-15 15:37:32 +0700

    Add Bootstrap

--- PyLearn/settings.py
+++ PyLearn/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
