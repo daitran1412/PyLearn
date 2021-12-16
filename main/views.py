69829130651bfad1308dd4b732783eb9e92f831d
hoangduong-hus <phamhoangduong_t64@hus.edu.vn>
2021-12-16 09:55:15 +0700

    Add pages

--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
-def index(request):
+def welcome(request):
     return render(request, 'main/welcome.html')
+
+def login(request):
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'main/login.html', context)
+
+def register(request):
+    return render(request, 'main/register.html')
