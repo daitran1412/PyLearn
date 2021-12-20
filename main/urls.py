643a17c1446a493822205d20819559569349d023
hoangduong-hus <phamhoangduong_t64@hus.edu.vn>
2021-12-20 11:22:29 +0700

    Done register, login, logout feature

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
-    path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('home/', views.home, name='home'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,49 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm
 
 # Create your views here.
 
 def welcome(request):
     return render(request, 'main/welcome.html')
 
-def login(request):
-    form = UserCreationForm()
-    context = {'form': form}
-    return render(request, 'main/login.html', context)
-
 def register(request):
-    return render(request, 'main/register.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Tạo tài khoản thành công!')
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'main/register.html', context)
+    
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                django_login(request, user)
+                messages.success(request, ('Đăng nhập thành công!'))
+                return redirect('home')
+            else:
+                messages.success(request, ('Tên tài khoản hoặc mật khẩu không đúng! Xin hãy thử lại!'))
+                return redirect('login')
+        return render(request, 'main/login.html')
+
+def logout(request):
+    django_logout(request)
+    return redirect('welcome')
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'main/home.html')
