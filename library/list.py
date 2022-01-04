b20361924319f9df20f765f3ff9ba2a566de7a43
hoangduong-hus <phamhoangduong_t64@hus.edu.vn>
2022-01-04 21:40:42 +0700

    Update problem list

--- library/list.py
+++ library/list.py
@@ -1,8 +1,14 @@
 Problems = [
     {
         "id": 1,
-        "title": "Fibonacci",
-        "description": "Lorem",
-        "starter": "def fibonacci(n)\n\tpass",
+        "title": "Character Input",
+        "description": "Mô tả bài tập",
+        "starter": "def correct_charInput(name, age):\n\tpass",
+    },
+    {
+        "id": 2,
+        "title": "Rock Paper Scissors",
+        "description": "Mô tả bài tập",
+        "starter": "def correct_compare(u1, u2):\n\tpass",
     }
 ]
